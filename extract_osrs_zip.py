ffbc65e743adc58e331047c48c55289be13088aa
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2022-01-02 01:37:18 +1100

    Update extract_osrs_zip.py

--- extract_osrs_zip.py
+++ extract_osrs_zip.py
@@ -1,8 +1,17 @@
 import os
+import pathlib
+import zipfile as z
+directory = pathlib.Path(__file__).parent.resolve()
 
-import zipfile
+zipPath = '\\datasets\\'
+dir_name = str(directory) + zipPath
+os.chdir(dir_name)
+print(str(directory))
 
-zipPath = './datasets/'
+first_cmd = 'cd ' + str(directory) + '\datasets\\'
+final_cmd = 'copy /B cow.zip.* output.zip'
 
-with zipfile.ZipFile(zipPath + 'osrs.zip*', 'r') as zip_ref:
-    zip_ref.extractall('./datasets/osrs/')
+os.system('cmd /c ' + first_cmd + ' & dir & ' + final_cmd)
+
+with z.ZipFile(str(directory) + "\datasets\output.zip", 'r') as zip_ref:
+    zip_ref.extractall(str(directory) + "\datasets\\")
