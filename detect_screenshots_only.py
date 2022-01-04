ba0546ba6b11272de5ee6cc157a10197ddd6cea3
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2022-01-04 15:53:11 +1100

    Update detect_screenshots_only.py

--- detect_screenshots_only.py
+++ detect_screenshots_only.py
@@ -1,15 +1,8 @@
 # YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-"""
-Run inference on images, videos, directories, streams, etc.
 
-Usage:
-    $ python detect.py --source img.jpg --weights yolov5s.pt --img 640
-    python detect.py --source aim_map_csgo0069.jpg --weights best.pt --img 1280
-"""
 import datetime
 import os
 import random
-import tensorflow as tf
 import pyautogui
 from PIL import ImageGrab
 import argparse
@@ -62,7 +55,6 @@ def click_object(box):
 
 @torch.no_grad()
 def run(weights='best.pt',  # model.pt path(s)
-        source='data/images',  # file/dir/URL/glob, 0 for webcam
         imgsz=640,  # inference size (pixels)
         conf_thres=0.7,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
@@ -77,7 +69,8 @@ def run(weights='best.pt',  # model.pt path(s)
         hide_labels=False,  # hide labels
         hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
-        Run_Duration_hours=6 # how long to run code for in hours
+        Run_Duration_hours=6, # how long to run code for in hours
+        Enable_clicks=True # will click on the class objects when confidence is greater than 90%
         ):
     t_end = time.time() + (60 * 60 * Run_Duration_hours)
     # using the datetime.fromtimestamp() function
@@ -142,7 +135,7 @@ def run(weights='best.pt',  # model.pt path(s)
                     #print('im0:', im0)
                     print('xyxy', xyxy)
                     #print('label:', label)
-                    if time.time() > time_clicked and float(conf) > 0.9:
+                    if time.time() > time_clicked and float(conf) > 0.9 and Enable_clicks:
                         click_object(xyxy)
                         time_clicked = time.time() + 10
                 # Print time (inference + NMS)
@@ -164,33 +157,9 @@ def run(weights='best.pt',  # model.pt path(s)
             start_time = time.time()
 
 
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640],
-                        help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.7, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=10, help='maximum detections per image')
-    parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', default=True, help='show results')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--line-thickness', default=1, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    return opt
-
 def main_auto():
     #check_requirements(exclude=('tensorboard', 'thop'))
     run(weights='best.pt',  # model.pt path(s)
-        source='data/images',  # file/dir/URL/glob, 0 for webcam
         imgsz=[640,640],  # inference size (pixels)
         conf_thres=0.7,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
@@ -205,16 +174,9 @@ def main_auto():
         hide_labels=False,  # hide labels
         hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
-        Run_Duration_hours=6  # how long to run code for in hours
+        Run_Duration_hours=6,  # how long to run code for in hours
+        Enable_clicks=False
         )
 
-def main(opt):
-    print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(opt).items()))
-    check_requirements(exclude=('tensorboard', 'thop'))
-    run(**vars(opt))
-
-
 if __name__ == "__main__":
-    opt = parse_opt()
-    #main(opt)
     main_auto()
